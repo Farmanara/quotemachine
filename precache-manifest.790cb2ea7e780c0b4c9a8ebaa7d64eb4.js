249d5056ee75658742a2a2f3ac18301df1aca3ae
Fighter <farmanara.code@gmail.com>
2020-10-02 01:56:32 +0330

    Updates

---
+++ precache-manifest.790cb2ea7e780c0b4c9a8ebaa7d64eb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8bfbc127ff8feab28ecebdaf136c54",
+    "url": "/quotemachine/index.html"
+  },
+  {
+    "revision": "61c3a7e1f403e6913c5c",
+    "url": "/quotemachine/static/css/main.b3c23b13.chunk.css"
+  },
+  {
+    "revision": "74a798f6899d5d5f59e2",
+    "url": "/quotemachine/static/js/2.32fffc12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotemachine/static/js/2.32fffc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c3a7e1f403e6913c5c",
+    "url": "/quotemachine/static/js/main.cbe54f65.chunk.js"
+  },
+  {
+    "revision": "6f723d7af196337f86c2",
+    "url": "/quotemachine/static/js/runtime-main.ace9baa9.js"
+  }
+]);
